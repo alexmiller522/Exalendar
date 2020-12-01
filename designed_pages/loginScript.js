c388933c369cf2d0e5dec516ec7a9559730e7e06
alexmiller522 <32317337+alexmiller522@users.noreply.github.com>
2020-12-01 03:37:29 -0500

    pictures
    Now the GitHub and RCOS logo are on the bottom of the screen. When they are clicked on they will open up a new tab with the github link to the project and the main rcos page. They need to be moved up higher on the page and preferably next to each other

--- designed_pages/loginScript.js
+++ designed_pages/loginScript.js
@@ -3,16 +3,16 @@ function changeWords1() {
     if (x.innerHTML == "EXALENDAR") {
         x.innerHTML = "An RCOS Project";
     } else if (x.innerHTML == "An RCOS Project") {
-        x.innerHTML = "An Open Source Project";
-    } else if (x.innerHTML == "An Open Source Project") {
+        x.innerHTML = "Open Source";
+    } else if (x.innerHTML == "Open Source") {
         x.innerHTML = "EXALENDAR";
     }
 }
 function changeWords2() {
     var x = document.getElementById("curr");
     if (x.innerHTML == "EXALENDAR") {
-        x.innerHTML = "An Open Source Project";
-    } else if (x.innerHTML == "An Open Source Project") {
+        x.innerHTML = "Open Source";
+    } else if (x.innerHTML == "Open Source") {
         x.innerHTML = "An RCOS Project";
     } else if (x.innerHTML == "An RCOS Project") {
         x.innerHTML = "EXALENDAR";
